f501aa848b1fa847bc19022fe477c312e2096379
romale007 <romale0819@gmail.com>
2021-06-16 20:16:15 +0300

    css corrections

--- app.js
+++ app.js
@@ -34,8 +34,9 @@ let BtnsPickerComponent = {
     return {
       show: false,
       weight: null,
+      titleOfSelected: null,
+      wholeInfo: null,
       ex3: {
-        label: 'Масса детали, грамм',
         val: 10,
         color: '#e99210',
         trackColor1: "#e99210",
@@ -43,41 +44,46 @@ let BtnsPickerComponent = {
       },
       qtypes: [
         {
-          type: "FDM",
+          isFDM: true,
           title: 'Низкое качество',
           description: 'Подойдет для крупных деталей...',
           layerHeight: '0.25 мм',
           costPer10g: 17,
-          imgUrl: 'https://s3-eu-west-1.amazonaws.com/3dhubs-knowledgebase/intro-fdm-3d-printing/6-fdm-sample-layers.jpg'
+          imgUrl: 'https://s3-eu-west-1.amazonaws.com/3dhubs-knowledgebase/intro-fdm-3d-printing/6-fdm-sample-layers.jpg',
+          unitTitle: 'Масса детали, грамм',
+          btnColor: 'warning'
         },
         {
-          type: "FDM",
+          isFDM: true,
           title: 'Среднее качество',
           description: 'Подойдет для деталей среднего размера...',
           layerHeight: '0.2 мм',
           costPer10g: 20,
-          imgUrl: 'https://s3-eu-west-1.amazonaws.com/3dhubs-knowledgebase/intro-fdm-3d-printing/6-fdm-sample-layers.jpg'
+          imgUrl: 'https://s3-eu-west-1.amazonaws.com/3dhubs-knowledgebase/intro-fdm-3d-printing/6-fdm-sample-layers.jpg',
+          unitTitle: 'Масса детали, грамм',
+          btnColor: 'warning'
         },
         {
-          type: "FDM",
+          isFDM: true,
           title: 'Высокое качество',
           description: 'Подойдет для мелких деталей...',
           layerHeight: '0.1 мм',
           costPer10g: 40,
-          imgUrl: 'https://s3-eu-west-1.amazonaws.com/3dhubs-knowledgebase/intro-fdm-3d-printing/6-fdm-sample-layers.jpg'
+          imgUrl: 'https://s3-eu-west-1.amazonaws.com/3dhubs-knowledgebase/intro-fdm-3d-printing/6-fdm-sample-layers.jpg',
+          unitTitle: 'Масса детали, грамм',
+          btnColor: 'warning'
         },
         {
-          type: "SLA",
+          isFDM: false,
           title: 'Наивысшее качество',
           description: 'Для очень мелких деталей...',
           layerHeight: '0.05 мм',
           costPer10g: 62,
-          imgUrl: 'https://s3-eu-west-1.amazonaws.com/3dhubs-knowledgebase/intro-fdm-3d-printing/6-fdm-sample-layers.jpg'
+          imgUrl: 'https://s3-eu-west-1.amazonaws.com/3dhubs-knowledgebase/intro-fdm-3d-printing/6-fdm-sample-layers.jpg',
+          unitTitle: 'Объем детали, мл',
+          btnColor: 'green'
         },
-      ],
-      titleOfSelected: null,
-      wholeInfo: null
-
+      ]
     }
   },
   methods: {
