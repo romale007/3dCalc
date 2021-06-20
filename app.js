65c2147206b1015e003075a2eaa072079236b9ff
romale007 <romale0819@gmail.com>
2021-06-20 19:01:29 +0300

    css updates

--- app.js
+++ app.js
@@ -38,15 +38,15 @@ let BtnsPickerComponent = {
       wholeInfo: null,
       ex3: {
         val: 10,
-        color: '#e99210',
-        trackColor1: "#e99210",
+        color: '#ffc107',
+        trackColor1: "#ffc107",
         trackColor2: "grey",
       },
       qtypes: [
         {
           isFDM: true,
           title: 'Низкое качество',
-          description: 'Подойдет для крупных деталей...',
+          description: 'Подойдет для крупных деталей, asdasdasdasdasdasdasd...',
           layerHeight: '0.25 мм',
           costPer10g: 17,
           imgUrl: '0.25fdm.jpg',
@@ -56,7 +56,7 @@ let BtnsPickerComponent = {
         {
           isFDM: true,
           title: 'Среднее качество',
-          description: 'Подойдет для деталей среднего размера...',
+          description: 'Подойдет для деталей среднего размера, asdasdasdasdasdasdasd...',
           layerHeight: '0.2 мм',
           costPer10g: 20,
           imgUrl: '0.20fdm.jpg',
@@ -66,7 +66,7 @@ let BtnsPickerComponent = {
         {
           isFDM: true,
           title: 'Высокое качество',
-          description: 'Подойдет для мелких деталей...',
+          description: 'Подойдет для мелких деталей, asdasdasdasdasdasdasd...',
           layerHeight: '0.1 мм',
           costPer10g: 40,
           imgUrl: '0.1fdm.jpg',
@@ -76,7 +76,7 @@ let BtnsPickerComponent = {
         {
           isFDM: false,
           title: 'Наивысшее качество',
-          description: 'Для очень мелких деталей...',
+          description: 'Для очень мелких деталей, asdasdasdasdasdasdasd...',
           layerHeight: '0.05 мм',
           costPer10g: 62,
           imgUrl: '0.05sla.jpg',
