28b3f83ef96d648b5bb63dc0aa4e35b0f87dc450
romale007 <romale0819@gmail.com>
2022-03-11 08:32:51 +0300

    webpack#2

--- notCDN/webpack.config.js
+++ notCDN/webpack.config.js
@@ -1,9 +1,24 @@
 const path = require('path')
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
 module.exports = {
     mode: 'development',
     entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
     module: {
-
+        rules: [{
+                test: /\.js$/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+            }
+        ],
     }
 }
